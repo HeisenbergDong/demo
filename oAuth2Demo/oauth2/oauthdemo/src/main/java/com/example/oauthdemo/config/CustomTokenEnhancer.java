28d3e53cb4f9f287dfe4816dfcb35a33b134d6b6
HeisenBergDong <HeisenBergDong@hotmail.com>
2019-05-26 11:50:30 +0800

    修改增强链内容

--- oAuth2Demo/oauth2/oauthdemo/src/main/java/com/example/oauthdemo/config/CustomTokenEnhancer.java
+++ oAuth2Demo/oauth2/oauthdemo/src/main/java/com/example/oauthdemo/config/CustomTokenEnhancer.java
@@ -12,7 +12,7 @@ public class CustomTokenEnhancer implements TokenEnhancer {
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
         Map<String, Object> additionalInfo = new HashMap<>();
-        additionalInfo.put("group", "yc");
+        additionalInfo.put("group", "oauth");
         ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
         return accessToken;
     }
